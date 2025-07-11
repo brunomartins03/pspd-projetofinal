aab8dd6b52b668fa2e76d7ee950db764b3b20aca
gitbmvb <bruno.martval@gmail.com>
2025-07-11 19:28:44 -0300

    feat/engine: realiza integração OpenMP + MPI

--- engine/mpi_openmp.c
+++ engine/mpi_openmp.c
@@ -1,109 +1,167 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#include <mpi.h>
 #include <omp.h>
+
 #define ind2d(i, j) ((i) * (size + 2) + (j))
+#define MASTER 0
+#define TAG 0
+
+typedef struct {
+    int    pow;
+    double t_init;
+    double t_comp;
+    double t_check;
+    double t_total;
+    int    correct;
+} result_t;
 
 double wall_time(void) {
     struct timeval tv;
-    struct timezone tz;
-
-    gettimeofday(&tv, &tz);
-    return (tv.tv_sec + tv.tv_usec / 1000000.0);
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec + tv.tv_usec * 1e-6;
 }
 
-void generation(int * matrixIn, int * matrixOut, int size) {
-    int neighbors_alive;
-
-    #pragma omp parallel for collapse(2) private(neighbors_alive) schedule(static)
-    for (int i = 1; i <= size; i++) {
-        for (int j = 1; j <= size; j++) {
-            neighbors_alive =
-                matrixIn[ind2d(i - 1, j - 1)] + matrixIn[ind2d(i - 1, j)] +
-                matrixIn[ind2d(i - 1, j + 1)] + matrixIn[ind2d(i, j - 1)] +
-                matrixIn[ind2d(i, j + 1)] + matrixIn[ind2d(i + 1, j - 1)] +
-                matrixIn[ind2d(i + 1, j)] + matrixIn[ind2d(i + 1, j + 1)];
-
-            if (matrixIn[ind2d(i, j)] && neighbors_alive < 2) matrixOut[ind2d(i, j)] = 0;
-            else if (matrixIn[ind2d(i, j)] && neighbors_alive > 3) matrixOut[ind2d(i, j)] = 0;
-            else if (!matrixIn[ind2d(i, j)] && neighbors_alive == 3) matrixOut[ind2d(i, j)] = 1;
-            else matrixOut[ind2d(i, j)] = matrixIn[ind2d(i, j)];
-        }
-    }
+void initialize_matrix(int *mat, int size) {
+    mat[ind2d(1, 2)] = 1;
+    mat[ind2d(2, 3)] = 1;
+    mat[ind2d(3, 1)] = 1;
+    mat[ind2d(3, 2)] = 1;
+    mat[ind2d(3, 3)] = 1;
 }
 
-void dump_matrix(int * matrix, int size, int first, int last, char * msg) {
-    int i, ij;
-
-    printf("%s; Dump posicoes [%d:%d, %d:%d] de tabuleiro %d x %d\n", msg, first, last, first, last, size, size);
-    for (i = first; i <= last; i++) printf("=");
-    printf("=\n");
-
-    for (i = ind2d(first, 0); i <= ind2d(last, 0); i += ind2d(1, 0)) {
-        for (ij = i + first; ij <= i + last; ij++) printf("%c", matrix[ij] ? 'X' : '.');
-        printf("\n");
-    }
-
-    for (i = first; i <= last; i++) printf("=");
-    printf("=\n");
-}
-
-void initialize_matrix(int* matrixIn, int* matrixOut, int size) {
-    matrixIn[ind2d(1, 2)] = 1;
-    matrixIn[ind2d(2, 3)] = 1;
-    matrixIn[ind2d(3, 1)] = 1;
-    matrixIn[ind2d(3, 2)] = 1;
-    matrixIn[ind2d(3, 3)] = 1;
-}
-
-int check_result(int * matrix, int size) {
+int check_result(int *mat, int size) {
     int cnt = 0;
-
-    #pragma omp parallel for reduction(+:cnt)
-    for (int ij = 0; ij < (size + 2) * (size + 2); ij++) cnt += matrix[ij];
-
-    return (cnt == 5 &&
-            matrix[ind2d(size - 2, size - 1)] &&
-            matrix[ind2d(size - 1, size)] &&
-            matrix[ind2d(size, size - 2)] &&
-            matrix[ind2d(size, size - 1)] &&
-            matrix[ind2d(size, size)]);
+    for (int ij = 0; ij < (size + 2)*(size + 2); ij++) cnt += mat[ij];
+    int ok = (cnt == 5 &&
+              mat[ind2d(size-2, size-1)] &&
+              mat[ind2d(size-1, size)]   &&
+              mat[ind2d(size,   size-2)] &&
+              mat[ind2d(size,   size-1)] &&
+              mat[ind2d(size,   size)]);
+    return ok;
 }
 
-int main(int argc, char * argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Argumentos inválidos.\nUso: %s <powmin> <powmax>\n", argv[0]);
-        exit(EXIT_FAILURE);
+void run_simulation(int pow, result_t *res) {
+    int size = 1 << pow;
+    int  total_steps = 2*(size - 3);
+    int *A = calloc((size+2)*(size+2), sizeof(int));
+    int *B = calloc((size+2)*(size+2), sizeof(int));
+    if (!A || !B) {
+        fprintf(stderr, "Allocation failed for size=%d\n", size);
+        MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
     }
 
-    int * matrixIn, * matrixOut;
-    int powmin = atoi(argv[1]), powmax = atoi(argv[2]);
-    char msg[9];
-    double t0, t1, t2, t3;
+    double t0 = wall_time();
+    initialize_matrix(A, size);
+    double t1 = wall_time();
 
-    for (int pow = powmin; pow <= powmax; pow++) {
-        int size = 1 << pow;
-
-        t0 = wall_time();
-        matrixIn  = (int*) calloc((size + 2) * (size + 2), sizeof(int));
-        matrixOut = (int*) calloc((size + 2) * (size + 2), sizeof(int));
-        initialize_matrix(matrixIn, matrixOut, size);
-        t1 = wall_time();
-
-        for (int i = 0; i < 2 * (size - 3); i++) {
-            generation(matrixIn, matrixOut, size);
-            generation(matrixOut, matrixIn, size);
+    #pragma omp parallel
+    {
+        int neighbors;
+        for (int step = 0; step < total_steps; ++step) {
+            #pragma omp for collapse(2) schedule(static) private(neighbors)
+            for (int i = 1; i <= size; ++i) {
+                for (int j = 1; j <= size; ++j) {
+                    neighbors =
+                        A[ind2d(i-1,j-1)] + A[ind2d(i-1,j)] + A[ind2d(i-1,j+1)] +
+                        A[ind2d(i,  j-1)]                  + A[ind2d(i,  j+1)] +
+                        A[ind2d(i+1,j-1)] + A[ind2d(i+1,j)] + A[ind2d(i+1,j+1)];
+                    if (A[ind2d(i,j)] && (neighbors < 2 || neighbors > 3))
+                        B[ind2d(i,j)] = 0;
+                    else if (!A[ind2d(i,j)] && neighbors == 3)
+                        B[ind2d(i,j)] = 1;
+                    else
+                        B[ind2d(i,j)] = A[ind2d(i,j)];
+                }
+            }
+            #pragma omp barrier
+            #pragma omp for collapse(2) schedule(static) private(neighbors)
+            for (int i = 1; i <= size; ++i) {
+                for (int j = 1; j <= size; ++j) {
+                    neighbors =
+                        B[ind2d(i-1,j-1)] + B[ind2d(i-1,j)] + B[ind2d(i-1,j+1)] +
+                        B[ind2d(i,  j-1)]                  + B[ind2d(i,  j+1)] +
+                        B[ind2d(i+1,j-1)] + B[ind2d(i+1,j)] + B[ind2d(i+1,j+1)];
+                    if (B[ind2d(i,j)] && (neighbors < 2 || neighbors > 3))
+                        A[ind2d(i,j)] = 0;
+                    else if (!B[ind2d(i,j)] && neighbors == 3)
+                        A[ind2d(i,j)] = 1;
+                    else
+                        A[ind2d(i,j)] = B[ind2d(i,j)];
+                }
+            }
+            #pragma omp barrier
         }
-        t2 = wall_time();
-
-        if (check_result(matrixIn, size)) printf("**RESULTADO CORRETO**\n");
-        else printf("**RESULTADO ERRADO**\n");
-
-        t3 = wall_time();
-
-        printf("tam=%d; tempos: init=%7.7f, comp=%7.7f, fim=%7.7f, tot=%7.7f\n", size, t1 - t0, t2 - t1, t3 - t2, t3 - t0);
-        free(matrixIn);
-        free(matrixOut);
     }
+
+    double t2 = wall_time();
+    int ok = check_result(A, size);
+    double t3 = wall_time();
+
+    res->pow     = pow;
+    res->t_init  = t1 - t0;
+    res->t_comp  = t2 - t1;
+    res->t_check = t3 - t2;
+    res->t_total = t3 - t0;
+    res->correct = ok;
+
+    free(A);
+    free(B);
+}
+
+int main(int argc, char *argv[]) {
+    MPI_Init(&argc, &argv);
+    int rank, nprocs;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+    if (argc != 3) {
+        if (rank == MASTER)
+            fprintf(stderr, "Usage: %s <powmin> <powmax>\n", argv[0]);
+        MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+    }
+
+    int powmin = atoi(argv[1]);
+    int powmax = atoi(argv[2]);
+    int nwork  = powmax - powmin + 1;
+
+    if (nprocs != nwork + 1) {
+        if (rank == MASTER) {
+            fprintf(stderr,
+                    "Error: expected %d worker ranks (pow=%d..%d), so mpirun -np %d\n",
+                    nwork, powmin, powmax, nwork+1);
+        }
+        MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+    }
+
+    if (rank != MASTER) {
+        int pow = powmin + rank - 1;
+        result_t myres;
+        run_simulation(pow, &myres);
+        MPI_Send(&myres, sizeof(myres), MPI_BYTE, MASTER, TAG, MPI_COMM_WORLD);
+    } else {
+        result_t *all = malloc(nwork * sizeof(result_t));
+        for (int src = 1; src <= nwork; ++src) {
+            MPI_Recv(&all[src-1], sizeof(result_t), MPI_BYTE,
+                     src, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+
+        printf(" pow |    init(s)  comp(s)    check(s)  total(s) | OK?\n");
+        printf("-----+-------------------------------------------------\n");
+        for (int i = 0; i < nwork; ++i) {
+            result_t *r = &all[i];
+            printf(" %3d | %9.6f  %9.6f  %9.6f  %9.6f |   %c\n",
+                   r->pow,
+                   r->t_init,
+                   r->t_comp,
+                   r->t_check,
+                   r->t_total,
+                   (r->correct ? 'Y' : 'N'));
+        }
+        free(all);
+    }
+    MPI_Finalize();
     return 0;
 }
