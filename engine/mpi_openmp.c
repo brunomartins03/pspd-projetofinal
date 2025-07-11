31c66bc2172792ef7f33b1c35d6612aa564cd4c5
gitbmvb <bruno.martval@gmail.com>
2025-07-11 15:26:53 -0300

    refac/engine: realiza refatorações no código sequencial

--- engine/mpi_openmp.c
+++ engine/mpi_openmp.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
-
-#define ind2d(i, j) ((i) * (tam + 2) + (j))
-#define POWMIN 3
-#define POWMAX 10
+#define ind2d(i, j) ((i) * (size + 2) + (j))
 
 double wall_time(void) {
     struct timeval tv;
@@ -14,41 +11,33 @@ double wall_time(void) {
     return (tv.tv_sec + tv.tv_usec / 1000000.0);
 }
 
-void UmaVida(int* tabulIn, int* tabulOut, int tam) {
+void generation(int * matrixIn, int * matrixOut, int size) {
     int i, j, vizviv;
-
-    for (i = 1; i <= tam; i++) {
-        for (j = 1; j <= tam; j++) {
+    for (i = 1; i <= size; i++) {
+        for (j = 1; j <= size; j++) {
             vizviv =
-                tabulIn[ind2d(i - 1, j - 1)] + tabulIn[ind2d(i - 1, j)] +
-                tabulIn[ind2d(i - 1, j + 1)] + tabulIn[ind2d(i, j - 1)] +
-                tabulIn[ind2d(i, j + 1)] + tabulIn[ind2d(i + 1, j - 1)] +
-                tabulIn[ind2d(i + 1, j)] + tabulIn[ind2d(i + 1, j + 1)];
+                matrixIn[ind2d(i - 1, j - 1)] + matrixIn[ind2d(i - 1, j)] +
+                matrixIn[ind2d(i - 1, j + 1)] + matrixIn[ind2d(i, j - 1)] +
+                matrixIn[ind2d(i, j + 1)] + matrixIn[ind2d(i + 1, j - 1)] +
+                matrixIn[ind2d(i + 1, j)] + matrixIn[ind2d(i + 1, j + 1)];
 
-            if (tabulIn[ind2d(i, j)] && vizviv < 2)
-                tabulOut[ind2d(i, j)] = 0;
-            else if (tabulIn[ind2d(i, j)] && vizviv > 3)
-                tabulOut[ind2d(i, j)] = 0;
-            else if (!tabulIn[ind2d(i, j)] && vizviv == 3)
-                tabulOut[ind2d(i, j)] = 1;
-            else
-                tabulOut[ind2d(i, j)] = tabulIn[ind2d(i, j)];
+            if (matrixIn[ind2d(i, j)] && vizviv < 2) matrixOut[ind2d(i, j)] = 0;
+            else if (matrixIn[ind2d(i, j)] && vizviv > 3) matrixOut[ind2d(i, j)] = 0;
+            else if (!matrixIn[ind2d(i, j)] && vizviv == 3) matrixOut[ind2d(i, j)] = 1;
+            else matrixOut[ind2d(i, j)] = matrixIn[ind2d(i, j)];
         }
     }
 }
 
-void DumpTabul(int* tabul, int tam, int first, int last, char* msg) {
+void dump_matrix(int * matrix, int size, int first, int last, char * msg) {
     int i, ij;
 
-    printf("%s; Dump posicoes [%d:%d, %d:%d] de tabuleiro %d x %d\n",
-           msg, first, last, first, last, tam, tam);
-
+    printf("%s; Dump posicoes [%d:%d, %d:%d] de tabuleiro %d x %d\n", msg, first, last, first, last, size, size);
     for (i = first; i <= last; i++) printf("=");
     printf("=\n");
 
     for (i = ind2d(first, 0); i <= ind2d(last, 0); i += ind2d(1, 0)) {
-        for (ij = i + first; ij <= i + last; ij++)
-            printf("%c", tabul[ij] ? 'X' : '.');
+        for (ij = i + first; ij <= i + last; ij++) printf("%c", matrix[ij] ? 'X' : '.');
         printf("\n");
     }
 
@@ -56,72 +45,66 @@ void DumpTabul(int* tabul, int tam, int first, int last, char* msg) {
     printf("=\n");
 }
 
-void InitTabul(int* tabulIn, int* tabulOut, int tam) {
-    int ij;
-
-    for (ij = 0; ij < (tam + 2) * (tam + 2); ij++) {
-        tabulIn[ij] = 0;
-        tabulOut[ij] = 0;
+void initialize_matrix(int* matrixIn, int* matrixOut, int size) {
+    for (int ij = 0; ij < (size + 2) * (size + 2); ij++) {
+        matrixIn[ij] = 0;
+        matrixOut[ij] = 0;
     }
 
-    tabulIn[ind2d(1, 2)] = 1;
-    tabulIn[ind2d(2, 3)] = 1;
-    tabulIn[ind2d(3, 1)] = 1;
-    tabulIn[ind2d(3, 2)] = 1;
-    tabulIn[ind2d(3, 3)] = 1;
+    matrixIn[ind2d(1, 2)] = 1;
+    matrixIn[ind2d(2, 3)] = 1;
+    matrixIn[ind2d(3, 1)] = 1;
+    matrixIn[ind2d(3, 2)] = 1;
+    matrixIn[ind2d(3, 3)] = 1;
 }
 
-int Correto(int* tabul, int tam) {
-    int ij, cnt = 0;
+int check_result(int * matrix, int size) {
+    int cnt = 0;
 
-    for (ij = 0; ij < (tam + 2) * (tam + 2); ij++)
-        cnt += tabul[ij];
+    for (int ij = 0; ij < (size + 2) * (size + 2); ij++) cnt += matrix[ij];
 
     return (cnt == 5 &&
-            tabul[ind2d(tam - 2, tam - 1)] &&
-            tabul[ind2d(tam - 1, tam)] &&
-            tabul[ind2d(tam, tam - 2)] &&
-            tabul[ind2d(tam, tam - 1)] &&
-            tabul[ind2d(tam, tam)]);
+            matrix[ind2d(size - 2, size - 1)] &&
+            matrix[ind2d(size - 1, size)] &&
+            matrix[ind2d(size, size - 2)] &&
+            matrix[ind2d(size, size - 1)] &&
+            matrix[ind2d(size, size)]);
 }
 
-int main(void) {
-    int pow, i, tam;
-    int *tabulIn, *tabulOut;
+int main(int argc, char * argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Argumentos inválidos.\nUso: %s <powmin> <powmax>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    int * matrixIn, * matrixOut;
+    int powmin = atoi(argv[1]), powmax = atoi(argv[2]);
     char msg[9];
     double t0, t1, t2, t3;
 
-    for (pow = POWMIN; pow <= POWMAX; pow++) {
-        tam = 1 << pow;
+    for (int pow = powmin; pow <= powmax; pow++) {
+        int size = 1 << pow;
 
         t0 = wall_time();
-        tabulIn  = (int*) malloc((tam + 2) * (tam + 2) * sizeof(int));
-        tabulOut = (int*) malloc((tam + 2) * (tam + 2) * sizeof(int));
-        InitTabul(tabulIn, tabulOut, tam);
+        matrixIn  = (int*) malloc((size + 2) * (size + 2) * sizeof(int));
+        matrixOut = (int*) malloc((size + 2) * (size + 2) * sizeof(int));
+        initialize_matrix(matrixIn, matrixOut, size);
         t1 = wall_time();
 
-        // DumpTabul(tabulIn, tam, 0, tam, "Teste");
-
-        for (i = 0; i < 2 * (tam - 3); i++) {
-            UmaVida(tabulIn, tabulOut, tam);
-            UmaVida(tabulOut, tabulIn, tam);
+        for (int i = 0; i < 2 * (size - 3); i++) {
+            generation(matrixIn, matrixOut, size);
+            generation(matrixOut, matrixIn, size);
         }
         t2 = wall_time();
 
-        if (Correto(tabulIn, tam))
-            printf("**RESULTADO CORRETO**\n");
-        else
-            printf("**RESULTADO ERRADO**\n");
+        if (check_result(matrixIn, size)) printf("**RESULTADO CORRETO**\n");
+        else printf("**RESULTADO ERRADO**\n");
 
         t3 = wall_time();
 
-        printf("tam=%d; tempos: init=%7.7f, comp=%7.7f, fim=%7.7f, tot=%7.7f\n",
-               tam, t1 - t0, t2 - t1, t3 - t2, t3 - t0);
-
-        // DumpTabul(tabulOut, tam, 0, tam, "Teste");
-
-        free(tabulIn);
-        free(tabulOut);
+        printf("tam=%d; tempos: init=%7.7f, comp=%7.7f, fim=%7.7f, tot=%7.7f\n", size, t1 - t0, t2 - t1, t3 - t2, t3 - t0);
+        free(matrixIn);
+        free(matrixOut);
     }
 
     return 0;
