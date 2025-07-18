a4863a9231c7f4d00e7e167a9fd8d11eeb531163
dougalvs <douglasvestos@gmail.com>
2025-07-18 20:46:51 -0300

    feat: atualiza código cliente para stress test

--- cliente.py
+++ cliente.py
@@ -1,76 +1,45 @@
 import socket
 import json
+import threading
+import time
+import random
 
-HOST = "192.168.49.2" # Ajustar conforme necessário
+HOST = "192.168.49.2"
 PORT = 30000
+NUM_CLIENTS = 10
+ENGINE = ["mpi", "spark"]
+POWS = [(3, 5), (3, 4), (4, 6), (5, 7)]
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+def client_task(id):
     try:
-        sock.connect((HOST, PORT))
-    except Exception as e:
-        print(f"Não foi possível conectar ao servidor em {HOST}:{PORT}. Erro: {e}")
-        exit(1)
-    
-    print(f"\nConectado ao host em: {HOST}:{PORT}\n")
-
-    print("#==================================================#\n")
-
-    try:
-        while True:
-            engine = ""
-            while engine not in ["mpi", "spark"]:
-                engine = input("Escolha a Engine (mpi/spark): ")
-                if engine not in ["mpi", "spark"]:
-                    print(f"Opção \"{engine}\" inválida. Tente novamente.\n")
-                    continue
-
-            if engine.lower() == "sair":
-                break
-            else :
-                print()
-            
-            powmin = powmax = ""
-            while powmin == "" or powmax == "":
-                powmin = input("POWMIN: ")
-                powmax = input("POWMAX: ")
-
-                if not powmin.isdigit() or not powmax.isdigit():
-                    print("POWMIN e POWMAX devem ser números inteiros. Tente novamente.\n")
-                    powmin = powmax = ""
-                    continue
-                elif int(powmin) < 0 or int(powmax) < 0:
-                    print("POWMIN e POWMAX devem ser números inteiros positivos. Tente novamente.\n")
-                    powmin = powmax = ""
-                    continue
-                elif int(powmin) > int(powmax):
-                    print(f"POWMIN ({powmin}) não pode ser maior que POWMAX ({powmax}). Tente novamente.\n")
-                    powmin = powmax = ""
-                    continue
-
-            print("\n||================================================||\n")
-
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.connect((HOST, PORT))
+            powmin, powmax = random.choice(POWS)
 
             payload = {
-                "engine": engine,
-                "powmin": int(powmin),
-                "powmax": int(powmax)
+                "engine": random.choice(ENGINE),
+                "powmin": powmin,
+                "powmax": powmax
             }
-
             sock.sendall(json.dumps(payload).encode())
             response = sock.recv(4096)
             response_data = json.loads(response.decode())
 
-            print(f"Status: {response_data.get('status', 'Erro')}")
-            print(f"Resposta do servidor:\n\n{response_data.get('data', 'Sem resultado')}")
-            print("#==================================================#\n")
-    except KeyboardInterrupt:
-        print("\n\n||================================================||\n")
-        print("Saindo.")
+            print(f"[Client {id}] Status: {response_data.get('status', 'Erro')}")
     except Exception as e:
-        print("\n||************************************************||\n")
-        print(f"Erro: {e}")
-    finally:
-        sock.close()
-        print("Conexão fechada.")
-    print("#==================================================#")
+        print(f"[Client {id}] Erro: {e}")
 
+
+threads = []
+start = time.time()
+
+for i in range(NUM_CLIENTS):
+    t = threading.Thread(target=client_task, args=(i,))
+    threads.append(t)
+    t.start()
+
+for t in threads:
+    t.join()
+
+end = time.time()
+print(f"\nFinalizado {NUM_CLIENTS} conexões em {end - start:.2f}s")
